ae551968a62007b0d4f81b3a88910a0cfa3e0c33
KangSangJun <befur1@g.hongik.ac.kr>
2026-08-06 02:22:30 +0900

    [chore] 프로젝트 초기 세팅

---
+++ src/main/java/com/_penLearning/Noddi/NoddiApplication.java
@@ -0,0 +1,13 @@
+package com._penLearning.Noddi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoddiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoddiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_penLearning/Noddi/NoddiApplicationTests.java
@@ -0,0 +1,13 @@
+package com._penLearning.Noddi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoddiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
