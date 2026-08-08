02460b3e86c0a24d5f54471eb0b6ca3d6e6eca41
KangSangJun <befur1@g.hongik.ac.kr>
2026-08-08 17:51:03 +0900

    [feat] 서비스/컨트롤러 중간 커밋

---
+++ src/main/java/com/_penLearning/Noddi/domain/meeting/controller/MeetingWebHookController.java
@@ -0,0 +1,8 @@
+package com._penLearning.Noddi.domain.meeting.controller;
+
+import lombok.extern.slf4j.Slf4j;
+
+//비동기 요청 확인 위한 로그 출력 목적 태그
+@Slf4j
+public class MeetingWebHookController {
+}
---
+++ src/main/java/com/_penLearning/Noddi/domain/meeting/service/MeetingService.java
@@ -0,0 +1,7 @@
+package com._penLearning.Noddi.domain.meeting.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MeetingService {
+}
