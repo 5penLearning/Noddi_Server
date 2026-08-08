e08b77f422eab09e6a56357cdfa6152dac4a3cc6
KangSangJun <befur1@g.hongik.ac.kr>
2026-08-08 23:26:16 +0900

    [feat] 데일리코 방 생성 변수 수정

--- src/main/java/com/_penLearning/Noddi/global/infrastructure/webRtc/DailyCoWebRtcClient.java
+++ src/main/java/com/_penLearning/Noddi/global/infrastructure/webRtc/DailyCoWebRtcClient.java
@@ -25,10 +25,25 @@ public class DailyCoWebRtcClient implements WebRtcClient{
         //방 이름 고유화
         String roomName = "noddi-" + UUID.randomUUID().toString().substring(0, 8);
 
+        //현재 시각 + 10분(600초) 후 Unix Timestamp(초 단위) 계산
+        long exp = (System.currentTimeMillis() / 1000) + 600;
         try {
             DailyCreateRoomResponseDto response = restClient.post()
                     .uri("/rooms")
-                    .body(Map.of("name", roomName, "privacy", "private", "properties", Map.of("enable_recording", "cloud")))
+                    .body(Map.of(
+                            "name", roomName,
+                            "privacy", "public",
+                            "properties", Map.of(
+                                    "enable_recording", "cloud",
+                                    "enable_hand_raising", true,     // 1. 손들기 기능 켜기
+                                    "enable_emoji_reactions", true,  // 2. 이모지 리액션 켜기
+                                    "enable_chat", true,             // 3. 텍스트 채팅창 켜기
+                                    "lang", "ko",                     // 4. 한국어 UI 설정
+                                    "max_participants", 10,           // 5. 최대 정원 10명 제한
+                                    "exp", exp,                       // 6. 10분 후 방 만료
+                                    "eject_at_room_exp", true         // 7. 만료 시 자동 강퇴
+                            )
+                    ))
                     .retrieve()
                     .body(DailyCreateRoomResponseDto.class);
 
