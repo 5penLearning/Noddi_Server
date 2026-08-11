7952dab4f392ce3c72f9e9c86d9ed0055a316d3b
hojun <jade5889@naver.com>
2026-08-11 10:07:30 +0900

    [refactor] 비밀번호 변경시 패턴 추가

--- src/main/java/com/_penLearning/Noddi/domain/user/dto/UserRequestDto.java
+++ src/main/java/com/_penLearning/Noddi/domain/user/dto/UserRequestDto.java
@@ -1,6 +1,7 @@
 package com._penLearning.Noddi.domain.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,7 +22,9 @@ public class UserRequestDto {
         private String currentPassword;
 
         @NotBlank(message = "새로운 비밀번호를 입력해주세요.")
-        // 정규식이 필요하다면 @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)...") 추가
-        private String newPassword;
+        @Pattern(
+                regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$",
+                message = "비밀번호는 8~20자의 영문, 숫자, 특수문자를 포함해야 합니다."
+        )        private String newPassword;
     }
 }
